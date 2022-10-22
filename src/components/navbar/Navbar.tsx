018662dd16ca9ffe495c9d8f688174ec30ea8f64
Pedro Andres Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2022-10-22 09:07:44 -0500

    Add fonts and create component folder

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,3 @@
+export function Navbar() {
+  return <h1>This is the amazing navbar</h1>;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+// Global styles
+import './global.css';
+
 // Components
+import { Navbar } from './components/navbar/Navbar';
 import { Starter } from './pages/Starter';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <Navbar />
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Starter />}></Route>
