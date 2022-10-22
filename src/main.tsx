a9a8106a71a9f6d928f2b1b5e2df354d831ce8b1
Pedro Andres Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2022-10-22 08:08:52 -0500

    Add eslint as dependency

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // Components
 import { Starter } from './pages/Starter';
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Starter/>}></Route>
+        <Route path='/' element={<Starter />}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
--- src/pages/Starter.tsx
+++ src/pages/Starter.tsx
@@ -1,3 +1,3 @@
-export function Starter(){
-  return <h1>Hello World</h1>
+export function Starter() {
+  return <h1>Hello World</h1>;
 }
