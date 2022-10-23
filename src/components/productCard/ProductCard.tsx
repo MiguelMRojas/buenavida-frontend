0838e0dad182e3ba485cba849e6636b1875f602c
Pedro Andres Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2022-10-23 09:21:08 -0500

    Hover affects and favorite icon

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -1,14 +1,19 @@
 import Styles from './ProductCard.module.css';
 import { Iproduct } from '../../interfaces/interfaces.ts';
+import { FiShoppingCart, FiHeart } from 'react-icons/fi';
 
 export function ProductCard(props: Iproduct) {
   console.log(props);
   return (
     <article className={Styles.product}>
+      <FiHeart className={Styles.product__heart} color={'red'} size={'1.4em'} />
       <img className={Styles.product__image} src={props.image} alt={props.name} />
       <h3 className={Styles.product__name}>{props.name}</h3>
       {props.units !== '' && <span className={Styles.product__units}>{props.units}</span>}
       <span className={Styles.product__price}>{props.price + '€'}</span>
+      <button className={Styles.product__button}>
+        <FiShoppingCart /> Add to cart
+      </button>
     </article>
   );
 }
