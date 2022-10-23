7a1c4bbb5c30fca6381809d896c88b81aa62f9b7
Pedro Andres Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2022-10-23 10:32:10 -0500

    Use environment variable as host

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { GLOBALS } from '../config/config.ts';
 
 export const getProducts = async () => {
-  const response = await axios.get('http://localhost:3030/products');
+  const response = await axios.get(`${GLOBALS.API_HOST}/products`);
   return response.data;
 };
